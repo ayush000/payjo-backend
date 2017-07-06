e3cf8ec96ab44e01f89719d7578a8a9138f6d680
Ayush Sachdeva <ayush@socialfrontier.com>
2017-07-06 22:03:27 +0530

    Login and register routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,44 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const User = mongoose.model('User')
 
 module.exports = {
-  async login (ctx) {
-    ctx.body = ctx.request.body
+  async login(ctx) {
+    const { body } = ctx.request
+    if (!body.user.email || !body.user.password) {
+      ctx.throw(
+        422,
+        'Email or password empty'
+      )
+    }
+    const user = await User.findOne({ email: body.user.email })
+    if (!user) {
+      ctx.throw(
+        401,
+        'Invalid user or password'
+      )
+    }
+    const passwordValid = await bcrypt.compare(body.user.password, user.password)
+    if (!passwordValid) {
+      ctx.throw(
+        401,
+        'Invalid user or password'
+      )
+    }
+    ctx.body = ctx.body = {
+      email: user.email,
+      token: user.generateJWT(),
+    }
   },
-  async register (ctx) {
+  async register(ctx) {
     const { body } = ctx.request
     let { user = {} } = body
+    if (!body.user.email || !body.user.password) {
+      ctx.throw(
+        422,
+        'Invalid email or password'
+      )
+    }
     const u = new User()
     u.email = user.email
     u.password = await bcrypt.hash(user.password, 10)
