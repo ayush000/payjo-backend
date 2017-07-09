2f107422c86286897015b96cbfe0becd8a5cba30
Ayush Sachdeva <ayush@socialfrontier.com>
2017-07-09 12:51:47 +0530

    Add cors, error handling

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
 const logger = require('koa-logger')
+const cors = require('kcors')
 require('./models/User')
 const routes = require('./routes')
 const app = new Koa()
@@ -10,7 +11,18 @@ mongoose.connect('mongodb://localhost/payjo')
 mongoose.Promise = global.Promise
 mongoose.set('debug', true)
 
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.status = err.status || 500
+    ctx.body = err.message
+    ctx.app.emit('error', err, ctx)
+  }
+})
+
 app
+  .use(cors())
   .use(logger())
   .use(bodyParser())
   .use(routes)
