ef87eeeac142085db5d4e8770aa077f82c10e610
Ayush Sachdeva <ayush@socialfrontier.com>
2017-07-10 14:28:29 +0530

    Add routes to update and delete product

--- controllers/product.js
+++ controllers/product.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 module.exports = {
   async create(ctx) {
-    const { body } = ctx.request
-    let { product = {} } = body
+    let { product = {} } = ctx.request.body
     console.log(product)
     if (!product.name ||
       !product.code ||
@@ -21,5 +20,31 @@ module.exports = {
   },
   async get(ctx) {
     ctx.body = await Product.find({})
+  },
+  async update(ctx) {
+    let { product = {} } = ctx.request.body
+    const { id } = ctx.params
+    if (!product.name ||
+      !product.code ||
+      !product.quantity ||
+      !product.expiry || !id) {
+      ctx.throw(
+        422,
+        'Invalid fields'
+      )
+    }
+
+    ctx.body = await Product.findByIdAndUpdate(id, { $set: product }, { new: true })
+  },
+  async delete(ctx) {
+    const { id } = ctx.params
+    if (!id) {
+      ctx.throw(
+        422,
+        'Invalid fields'
+      )
+    }
+    await Product.findByIdAndRemove(id)
+    ctx.body = {}
   },
 }
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,7 @@ const { product } = require('../controllers')
 
 router.post('/products', product.create)
 router.get('/products', product.get)
+router.put('/products/:id', product.update)
+router.del('/products/:id', product.delete)
 
 module.exports = router.routes()
